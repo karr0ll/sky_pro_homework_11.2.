5f19116538c326bb307e95f729aef57464582f8e
Kirill Tuchkin <volkirtu@gmail.com>
2022-12-14 19:31:06 +0300

    Homework_10.1_v.1 created

--- homework_10.1./main.py
+++ homework_10.1./main.py
@@ -3,17 +3,19 @@ from utils import load_candidates, get_candidates_list
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def start_page():
     table = get_candidates_list()
     return table
 
-@app.route("/candidate/<int:id>")
-def candidate_page(id):
+
+@app.route("/candidate/<int:id_>")
+def candidate_page(id_):
     candidates = load_candidates()
     profile = ""
     for candidate in candidates:
-        if candidate["id"] == id:
+        if candidate["id"] == id_:
             profile += f"""<img src="{candidate["picture"]}">\n"""
             profile += "<pre>"
             profile += f"""
@@ -24,11 +26,11 @@ def candidate_page(id):
     profile += "</pre>"
     return profile
 
+
 @app.route("/skill/<skill>")
 def candidate_skill_page(skill):
     candidates = load_candidates()
     profile = ""
-    #skill_l = skill.lower()
     for candidate in candidates:
         if skill.lower() in candidate["skills"].lower():
             profile += "<pre>"
@@ -40,5 +42,5 @@ def candidate_skill_page(skill):
         profile += "</pre>"
     return profile
 
-app.run()
 
+app.run()
